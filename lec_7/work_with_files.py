a34900f9df22ddf5a5c2aa11299e66fc90bf2c61
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2023-10-25 00:28:51 +0400

    Added and changed some error handling

--- lec_7/work_with_files.py
+++ lec_7/work_with_files.py
@@ -1,19 +1,44 @@
+import time
+
 def fileName():
     filename = input("Please enter .txt file: ")
-    if filename.lower().endswith(".txt"):
-        name_without_extension=filename[:-4]
-        if name_without_extension.isnumeric():
-            try:
-                raise ValueError('Invalid Filename: Name is numeric.')
-            except ValueError as error:
-                print(error)
-        else:
-            try:
-                with open(filename,"r") as f:
-                    print(f.read())
-            except FileNotFoundError:
-                print("File not found. \nIf you're sure that the file exists, please provide it's path.\nFor example C:/User/Asrock/Desktop/baba.txt")
-    else:
-        print("Extension must be .txt")
+    #if filename.lower().endswith(".txt"):
+    try:
+        with open(filename,"r") as f:
+            print(f.read())
+            time.sleep(3)
+            answer=input("\nDo you want to write in this file or create new file?\nType yes if you want to write in this file, else type no\n")
+            if answer.lower()=="yes":
+                with open(filename,"a") as f:
+                    f.write("\n"+input("Enter text to add at the end of the document: "))
+                    f.close()
+                    print("Successfully closed the file")
+            else:
+                new_file=input("Input the name of file you want to create.\n")
+                if new_file.isnumeric():
+                    try:
+                        raise ValueError('Invalid Filename: Name is numeric.')
+                    except ValueError as error:
+                            print(error)
+                else:
+                    if new_file.lower().endswith(".txt"):
+                        try:
+                            raise SyntaxWarning("No need to write the extension")
+                        except SyntaxWarning as warning:
+                            print(warning)
+                    try:
+                        with open(new_file,"x") as f:
+                            f.write("\n"+input("\nEnter text to add at the end of the document: "))
+                    except FileExistsError:
+                        print("File already exists.")
+                    else:
+                        print("\nThe writing is successful")
+                    finally:
+                        f.close()
+                        print("The file is closed")
+    except FileNotFoundError:
+        print("File not found. \nIf you're sure that the file exists, please provide it's path.\nFor example C:/User/Asrock/Desktop/baba.txt")
+""" else:
+    print("Extension must be .txt") """
 
 fileName()
