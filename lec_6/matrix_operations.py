70fe60192a9da81c29b58060f102a811e1ee5e0a
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2023-10-27 01:01:08 +0400

    Better solution

--- lec_6/matrix_operations.py
+++ lec_6/matrix_operations.py
@@ -8,30 +8,32 @@ def generate_random_matrix(rows,cols):
             row.append(random.randrange(1, 100))
         random_matrix.append(row)
     return random_matrix """
-    return [[random.randint(1,100) for _ in range(cols)] for _ in range(rows)]
-        
+    matrix = [[random.randint(1,100) for _ in range(cols)] for _ in range(rows)]
+    for row in matrix:
+        print(row)
+    return matrix
+
 def get_column_sum(matrix_2d, col_index):
     sum=0
     if 0 <= col_index < len(matrix_2d[0]):
-        for i in range(len(matrix_2d)):
-            print(matrix_2d[i][col_index])
-            sum+=matrix_2d[i][col_index]
+        for row in matrix_2d:
+            sum += row[col_index]
+        return f"Sum of column {col_index} is {sum}"
     else:
         print("Invalid column index.")
-    return sum
+
 
 def get_row_average(matrix_2d,row_index):
     avg = 0
     if 0 <= row_index < len(matrix_2d):
-        for j in range(len(matrix_2d[0])):
-            print(matrix_2d[row_index][j])
-            avg+=matrix_2d[row_index][j]/2
+        row=matrix_2d[row_index]
+        avg = sum(row)/2
+        return f"Average of row {row_index} is {avg}"
     else:
         print("Invalid row index.")
-    return avg
+    
 
 matrix_2d=generate_random_matrix(5,4)
-print(matrix_2d)
 col_index=int(input("Enter the index of column: "))
 print(get_column_sum(matrix_2d, col_index))
 row_index=int(input("Enter the index of row: "))
