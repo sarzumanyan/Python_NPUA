f106d8c0f14617b9b1a88e86c04278717882ea89
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2023-11-09 20:06:06 +0400

    Better calculator

--- lec_3/calculator.py
+++ lec_3/calculator.py
@@ -1,22 +1,26 @@
 exp = input("Enter any expression: ")
 op = ["*", "+", "-", "/", "^"]
 
-if "+" in exp:
-    result = exp.split("+")
-    result = float(result[0]) + float(result[1])
-elif "-" in exp:
-    result = exp.split("-")
-    result = float(result[0]) - float(result[1])
-elif "*" in exp:
-    result = exp.split("*")
-    result = float(result[0]) * float(result[1])
-elif "/" in exp:
-    result = exp.split("/")
-    result = float(result[0]) / float(result[1])
-# elif "^" in exp:
-#     result = exp.split("^")
-#     result = float(result[0]) ** float(result[1])
-else:
-    result = "This calculator does not support that expression))\nYet"
-print(result)
+def calculator():
+    if "+" in exp:
+        result = exp.split("+")
+        result = float(result[0]) + float(result[1])
+    elif "-" in exp:
+        result = exp.split("-")
+        result = float(result[0]) - float(result[1])
+    elif "*" in exp:
+        result = exp.split("*")
+        result = float(result[0]) * float(result[1])
+    elif "/" in exp:
+        result = exp.split("/")
+        try:
+            result = float(result[0]) / float(result[1])
+        except ZeroDivisionError:
+           return "Cannot divide by zero."
+    else:
+        result = "This calculator does not support that expression))\nYet"
+    return result
+
+if __name__=="__main__":
+    print(calculator())
 
