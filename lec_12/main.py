08d43bd89904082276eccfe78ed133c166a7dbc3
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2023-12-01 00:00:45 +0400

    lec_12

--- lec_12/main.py
+++ lec_12/main.py
@@ -11,7 +11,6 @@ def measure_execution_time(func):
         return result
     return wrapper
 
-# Function to generate random numbers and write them to a file
 def generate_and_write_numbers(filename, num_lines=100, num_per_line=20):
     with open(filename, 'w') as file:
         for _ in range(num_lines):
@@ -19,8 +18,6 @@ def generate_and_write_numbers(filename, num_lines=100, num_per_line=20):
             line = ' '.join(map(str, numbers))
             file.write(line + '\n')
 
-# Function to read lines from a file, convert them to integer arrays, filter numbers > 40, and write back to the file
-@measure_execution_time
 def process_file(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -31,24 +28,18 @@ def process_file(filename):
         for line in processed_lines:
             file.write(' '.join(map(str, line)) + '\n')
 
-# Generator function to yield lines from a file
 def read_file_generator(filename):
     with open(filename, 'r') as file:
         for line in file:
             yield list(map(int, line.split()))
 
-# Example usage
 filename = 'random_nums.txt'
 
-# Step 1: Generate and write random numbers to a file
 generate_and_write_numbers(filename)
 
-# Step 2: Process the file and measure execution time using the decorator
 process_file(filename)
 
-# Step 3: Read the file using a generator
 gen = read_file_generator(filename)
 
-# Step 4: Print the lines yielded by the generator
 for line in gen:
     print(line)
