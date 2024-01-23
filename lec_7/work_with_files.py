f170c2633c543e92e05d4dea254bc1dc156ded83
sarzumanyan <sona.arzumanyan42@gmail.com>
2024-01-23 23:41:29 +0400

    Syntax fixing

--- lec_7/work_with_files.py
+++ lec_7/work_with_files.py
@@ -7,14 +7,14 @@ def fileName():
         with open(filename,"r") as f:
             print(f.read())
             time.sleep(3)
-            answer=input("\nDo you want to write in this file or create new file?\nType yes if you want to write in this file, else type no\n")
-            if answer.lower()=="yes":
+            answer = input("\nDo you want to write in this file or create new file?\nType yes if you want to write in this file, else type no\n")
+            if answer.lower() == "yes":
                 with open(filename,"a") as f:
-                    f.write("\n"+input("Enter text to add at the end of the document: "))
+                    f.write("\n"+ input("Enter text to add at the end of the document: "))
                     f.close()
                     print("Successfully closed the file")
             else:
-                new_file=input("Input the name of file you want to create.\n")
+                new_file = input("Input the name of file you want to create.\n")
                 if new_file.isnumeric():
                     try:
                         raise ValueError('Invalid Filename: Name is numeric.')
@@ -28,7 +28,7 @@ def fileName():
                             print(warning)
                     try:
                         with open(new_file,"x") as f:
-                            f.write("\n"+input("\nEnter text to add at the end of the document: "))
+                            f.write("\n" + input("\nEnter text to add at the end of the document: "))
                     except FileExistsError:
                         print("File already exists.")
                     else:
